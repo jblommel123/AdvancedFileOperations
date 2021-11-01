608f1ea2203f9ef01f780652cee8b01fe397d0fc
Joe Blommel <j-blommel@hotmail.com>
2021-11-01 10:26:15 -0400

    setup basic program stucture

--- AdvancedFileOperations/AdvancedFileOperations.cpp
+++ AdvancedFileOperations/AdvancedFileOperations.cpp
@@ -4,33 +4,62 @@
 #include <iostream>
 #include <fstream>
 
-
 int showMenu();
+void displayMenuChoices();
+int getRecordNumber();
 void createFile(std::fstream&);
 void displayFile(std::fstream&);
 void displayRecord(std::fstream&, int);
-
+void modifyAProduct(std::fstream&, int);
 
 
 int main()
 {
     std::fstream fileStream;
-    fileStream.open("inventory.dat", std::ios::out);
+    
+    createFile(fileStream);
+    int menuFunction = 0;
+
+    do {
+        menuFunction = showMenu();
+        switch (menuFunction)
+        {
+        case 1: displayFile(fileStream);
+            break;
+        case 2: displayRecord(fileStream, getRecordNumber());
+            break;
+        case 3: modifyAProduct(fileStream, getRecordNumber());
+            break;
+        case 4: exit(0);
+            break;
+        default:
+            break;
+        }
+    } while (menuFunction != 4);
+
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-
 int showMenu() {
+    std::cout << "Please pick a menu option from the choices below." << std::endl;
+    displayMenuChoices();
+    int menuChoice;
+    std::cin >> menuChoice;
+    while (menuChoice < 1 || menuChoice > 5)
+    {
+        std::cout << "That is not a menu choice. Please enter a choice between 1 and 4." << std::endl;
+        displayMenuChoices();
+        std::cin >> menuChoice;
+    }
 
+    return menuChoice;
+}
+
+void displayMenuChoices() {
+    std::cout << "1. Display the entire inventory" << std::endl;
+    std::cout << "2. Display a particular product" << std::endl;
+    std::cout << "3. Modify a product" << std::endl;
+    std::cout << "4. Exit" << std::endl;
 }
 void createFile(std::fstream& stream) {
     const int NAME_SIZE = 40;
@@ -41,16 +70,55 @@ void createFile(std::fstream& stream) {
         int quantity;
     };
 
+    stream.open("inventory.dat", std::ios::out);
     Product p1;
     p1.number = 1;
     strcpy(p1.name, "Giant TCR Advanced Pro 1");
     p1.price = 5000.00;
     p1.quantity = 10;
+    stream.write(reinterpret_cast<char *>(&p1), sizeof(p1));
+
+    Product p2;
+    strcpy(p2.name, "Cannondale SystemSix");
+    p2.number = 2;
+    p2.price = 7500.00;
+    p2.quantity = 5;
+    stream.write(reinterpret_cast<char *>(&p2), sizeof(p2));
+
+    Product p3;
+    p3.number = 3;
+    strcpy(p3.name, "Colnago C64");
+    p3.price = 12000.00;
+    p3.quantity = 2;
+    stream.write(reinterpret_cast<char *>(&p3), sizeof(p3));
+
+    Product p4;
+    strcpy(p4.name, "Winspace T1500");
+    p4.number = 4;
+    p4.price = 2500;
+    p4.quantity = 25;
+    stream.write(reinterpret_cast<char *>(&p4), sizeof(p4));
+
+        std::cout << sizeof(p1) << std::endl;
+        std::cout << sizeof(p2) << std::endl;
+        std::cout << sizeof(p3) << std::endl;
+        std::cout << sizeof(p4) << std::endl;
 
 }
-void displayRecord(std::fstream& stream, int recordNumber)
-{
+//void displayRecord(std::fstream& stream, int recordNumber)
+//{
+//    int byteNumber = recordNumber - 1;
+//    stream.seekg(byteNum(byteNumber),std::ios::beg);
+//}
 
+
+int getRecordNumber() {
+    std::cout << "Please enter the record number you would like to see." << std::endl;
+    int recordNumber;
+    std::cin >> recordNumber;
+    return recordNumber;
 }
 
-
+long byteNum(int recordNumber) {
+    return (40 * recordNumber);
+}
